06c47dbab6366d9e485838969922e301340f54c6
fyneloveday <jbfyne18@yahoo.com>
2019-03-06 16:16:31 -0600

    Created a createRolesAndUsers method in the Startup Class, and changed the code in the Register.cshtml class to add textbox and combobox with caption

--- TrashCollector/TrashCollector/Startup.cs
+++ TrashCollector/TrashCollector/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using TrashCollector.Models;
 
 [assembly: OwinStartupAttribute(typeof(TrashCollector.Startup))]
 namespace TrashCollector
@@ -12,11 +15,34 @@ namespace TrashCollector
             createRolesandUsers();
         }
 
+
+        // In this method we will create default User roles and Admin user for login   
         private void createRolesandUsers()
         {
-            ApplicationDbContext context = new PplicationDbContext();
+            ApplicationDbContext context = new ApplicationDbContext();
 
-            var roleManager = new Role Manager<IdentityRole
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // creating Creating Manager role    
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+
+            }
         }
+
     }
 }
