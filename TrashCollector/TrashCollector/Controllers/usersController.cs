811b2411eb5ff6fed6999b0e8dca983ac8553181
fyneloveday <jbfyne18@yahoo.com>
2019-03-07 11:01:21 -0600

    Added an Index view

--- TrashCollector/TrashCollector/Controllers/usersController.cs
+++ TrashCollector/TrashCollector/Controllers/usersController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
+
+    [Authorize]
     public class usersController : Controller
     {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        ApplicationDbContext context;
         // GET: users
         public ActionResult Index()
         {
