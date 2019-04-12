6434b093de8eb4b1c232dabd7f4bbd7cbf7ee543
fyneloveday <jbfyne18@yahoo.com>
2019-04-12 14:42:26 -0500

    Created Payment model

---
+++ TrashCollector/TrashCollector/Models/Payment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public string Price { get; set; }
+    }
+}
